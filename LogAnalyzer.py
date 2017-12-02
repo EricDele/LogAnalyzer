62187fe669adf646219496ce80b32526aeabff79
Eric Deleforterie <eric@deleforterie.com>
2017-12-02 23:24:56 +0100

    Switch to Object and add the stats and logins update and report when exiting

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,9 +1,38 @@
-#! /usr/bin/env python
+#! /usr/bin/python
+# coding: utf-8
+
+# ===================================================================#
+# -------------------------------------------------------------------#
+#                             LogAnalyzer                            #
+# -------------------------------------------------------------------#
+# *******************************************************************#
+#                   Eric Deleforterie - 2017/12/01                   #
+# -------------------------------------------------------------------#
+#                          Notes/Comments                            #
+#                                                                    #
+# -------------------------------------------------------------------#
+#                             HISTORY                                #
+#    V0.0.1    Eric Deleforterie - 2017/12/01                        #
+#              Creation and first features                           #
+# ===================================================================#
+
+
+# --------------------------------------------#
+#             Packages Importation            #
+# --------------------------------------------#
+from __future__ import print_function
 import os
+import sys
 import re
 from datetime import datetime
 import time
+import json
+from collections import defaultdict
+from pprint import pprint
 
+# --------------------------------------------#
+#              Variables declaration          #
+# --------------------------------------------#
 
 DEFAULT = '\033[39m'
 BLACK = '\033[30m'
@@ -19,78 +48,152 @@ BACK_BLUE = '\033[44m'
 BACK_CYAN = '\033[46m'
 BACK_GRAY = '\033[47m'
 BACK_DEFAULT = '\033[49m'
-dateTimeFormat = '%Y-%m-%d %H:%M:%S.%f'
-thresholdLoginMinutes = 1
-lastTime = actualTime = datetime.now()
+
+global vg_configuration
 
 
-# 2017-11-30 18:48:39.607 26538 LOG Stats: 209 req/s, in 560744 b/s, out 660303 b/s,query 11209 us
-# 2017-11-30 17:38:23.156 26538 LOG C-0x12311d0: hue/hue@192.246.33.6:33224 login attempt: db=hue user=hue tls=no
-# 2017-11-30 18:48:39.732 26538 LOG C-0x1242970: hue/hue@192.246.33.7:44679 closing because: client close request (age=12)
+class LogAnalyzer():
 
-iter_lines = re.compile(r"^(?P<logTime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) (?P<pid>\d+) LOG (?P<lineType>Stats|C-0x[0-9a-fA-F]+): (?P<lineDetail>.*)$", re.M).finditer
-iter_line_detail_Stats = re.compile(r"^(?P<logReqBySec>\d+) req/s, in (?P<logBytesInBySec>\d+) b/s, out (?P<logBytesOutBySec>\d+) b/s,query (?P<logReqMicroSec>\d+) us$",re.M).finditer
-iter_line_detail_Login = re.compile(r"^(?P<logUser>\w+)/(?P<logDatabase>\w+)@(?P<logIpFrom>\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}):(?P<logPortFrom>\d+) (?P<logActionType>closing|login) (?:because|attempt): (?:db=(?P<db>\w+) user=(?P<user>\w+) tls=(?P<tls>\w+)|).*$",re.M).finditer
+    def __init__(self, jsonConfiguration):
+        self.configuration = jsonConfiguration['configuration']
+        self.dateTimeFormat = self.configuration['dateTimeFormat']
+        self.tresholdAlertingInMinutes = self.configuration['tresholdAlertingInMinutes']
+        self.windowDateTimeFormat = self.configuration['windowDateTimeFormat']
+        self.windowOpenServiceDateTime = datetime.time(datetime.strptime(self.configuration['windowOpenServiceDateTime'], self.configuration['windowDateTimeFormat']))
+        self.windowCloseServiceDateTime = datetime.time(datetime.strptime(self.configuration['windowCloseServiceDateTime'], self.configuration['windowDateTimeFormat']))
+        self.trailingDelay = float(self.configuration['trailingDelay'])
+        self.logins = defaultdict(dict)
+        self.stats = defaultdict(dict)
+        self.file = jsonConfiguration['file']
+        self.logFileDateTimeFormat = self.file['logFileDateTimeFormat']
+        self.iterLines = re.compile(r"{}".format(self.file['regexp']['iterLines']), re.M).finditer
+        self.iterLinesDetailStats = re.compile(r"{}".format(self.file['regexp']['iterLinesDetailStats']), re.M).finditer
+        self.iterLinesDetailLogin = re.compile(r"{}".format(self.file['regexp']['iterLinesDetailLogin']), re.M).finditer
 
-def followingFile(file, delay = 1.0):
-    line_terminators = ('\r\n', '\n', '\r')
-    trailing = True
-    while 1:
-        whereWeAre = file.tell()
-        line = file.readline()
-        if line:
-            if trailing and line in line_terminators:
-                # This is just the line terminator added to the end of the file
-                # before a new line, ignore.
-                trailing = False
-                continue
-
-            if line[-1] in line_terminators:
-                line = line[:-1]
-                if line[-1:] == '\r\n' and '\r\n' in line_terminators:
-                    # found crlf
+    def followingFile(self, file):
+        line_terminators = ('\r\n', '\n', '\r')
+        trailing = True
+        while True:
+            whereWeAre = file.tell()
+            line = file.readline()
+            if line:
+                if trailing and line in line_terminators:
+                    # This is just the line terminator added to the end of the file
+                    # before a new line, ignore.
+                    trailing = False
+                    continue
+                if line[-1] in line_terminators:
                     line = line[:-1]
+                    if line[-1:] == '\r\n' and '\r\n' in line_terminators:
+                        # found crlf
+                        line = line[:-1]
+                trailing = False
+                yield line
+            else:
+                trailing = True
+                file.seek(whereWeAre)
+                time.sleep(self.trailingDelay)
 
-            trailing = False
-            yield line
-        else:
-            trailing = True
-            file.seek(whereWeAre)
-            time.sleep(delay)
+    def updateStats(self, lineReqBySec, lineBytesInBySec, lineBytesOutBySec, lineReqMicroSec):
+        if('requestsBySeconds' not in self.stats):
+            self.stats['requestsBySeconds'] = []
+            self.stats['bytesInBySeconds'] = []
+            self.stats['bytesOutBySeconds'] = []
+            self.stats['requestDurationInSeconds'] = []
+        self.stats['requestsBySeconds'].append(int(lineReqBySec))
+        self.stats['bytesInBySeconds'].append(int(lineBytesInBySec))
+        self.stats['bytesOutBySeconds'].append(int(lineBytesOutBySec))
+        self.stats['requestDurationInSeconds'].append(float(lineReqMicroSec) / 1000000)
 
+    def printStats(self):
+        if('requestsBySeconds' in self.stats):
+            print(BLUE + "\nStats for running periode from " + DEFAULT + self.startTime + BLUE +
+                  " to " + DEFAULT + datetime.now().strftime(self.dateTimeFormat) + BLUE + " : \n" +
+                  CYAN + "Average requests by second : " + YELLOW + str(reduce(lambda x, y: x + y, self.stats['requestsBySeconds']) / len(self.stats['requestsBySeconds'])) + "\n" +
+                  CYAN + "Average bytes IN by second : " + YELLOW + str(reduce(lambda x, y: x + y, self.stats['bytesInBySeconds']) / len(self.stats['bytesInBySeconds'])) + "\n" +
+                  CYAN + "Average bytes OUT by second : " + YELLOW + str(reduce(lambda x, y: x + y, self.stats['bytesOutBySeconds']) / len(self.stats['bytesOutBySeconds'])) + "\n" +
+                  CYAN + "Average request duration by second : " + YELLOW + str(reduce(lambda x, y: x + y, self.stats['requestDurationInSeconds']) / len(self.stats['requestDurationInSeconds'])) +
+                  DEFAULT)
 
-def analyzeLog(lastTime, logTime, logUser, logDatabase, logIpFrom, logPortFrom, logActionType, db, user, tls):
-    logDateTime = datetime.strptime(logTime,dateTimeFormat)
-    timeDiff = logDateTime - lastTime
-    if(((timeDiff.days * 24 * 60) + (timeDiff.seconds/60)) > thresholdLoginMinutes ):
-        print(RED + "User " + user + " no new connection since " + str(thresholdLoginMinutes) + " minutes : " + str(lastTime)) 
-    lastTime = logDateTime
+    def updateLogin(self, lineLogTime, lineDatabase, lineUser, lineIpFrom, linePortFrom, lineActionType, lineDbCx, lineUserCx, lineTls):
+        if(lineUserCx not in self.logins):
+            self.logins[lineUserCx]['firstLoginTime'] = datetime.strptime(lineLogTime, self.file['logFileDateTimeFormat'])
+            self.logins[lineUserCx]['numberOfLogins'] = 0
+        if('ipFrom' not in self.logins[lineUserCx]):
+            self.logins[lineUserCx]['ipFrom'] = defaultdict(dict)
+        if(lineIpFrom not in self.logins[lineUserCx]['ipFrom']):
+            self.logins[lineUserCx]['ipFrom'][lineIpFrom] = 0
+        self.logins[lineUserCx]['lastLoginTime'] = datetime.strptime(lineLogTime, self.file['logFileDateTimeFormat'])
+        self.logins[lineUserCx]['numberOfLogins'] += 1
+        self.logins[lineUserCx]['ipFrom'][lineIpFrom] += 1
+
+    def printLogins(self):
+        print(BLUE + "\nStats for logins, periode from " + DEFAULT + self.startTime + BLUE +
+              " to " + DEFAULT + datetime.now().strftime(self.dateTimeFormat) + BLUE + " : \n")
+        for account in self.logins:
+            print(BLUE + "Account : " + GREEN + account)
+            print(CYAN + "First login time : " + YELLOW + self.logins[account]['firstLoginTime'].strftime(self.dateTimeFormat) + "\n" +
+                  CYAN + "Last login time : " + YELLOW + self.logins[account]['lastLoginTime'].strftime(self.dateTimeFormat) + "\n" +
+                  CYAN + "Number of logins : " + YELLOW + str(self.logins[account]['numberOfLogins']))
+            for ipFrom in self.logins[account]['ipFrom']:
+                print(CYAN + "Number of logins from " + YELLOW + ipFrom + CYAN + " : " + YELLOW + str(self.logins[account]['ipFrom'][ipFrom]) + DEFAULT)
+
+    # def analyzeLog(self,lastTime, logTime, logUser, logDatabase, logIpFrom, logPortFrom, logActionType, db, user, tls):
+    #     logDateTime = datetime.strptime(logTime, dateTimeFormat)
+    #     timeDiff = logDateTime - lastTime
+    #     if(((timeDiff.days * 24 * 60) + (timeDiff.seconds/60)) > thresholdLoginMinutes ):
+    #         print(RED + "User " + user + " no new connection since " + str(thresholdLoginMinutes) + " minutes : " + str(lastTime))
+    #     lastTime = logDateTime
+
+    def start(self):
+        self.startTime = datetime.now().strftime(self.dateTimeFormat)
+        print(BLUE + "Start date : " + DEFAULT + self.startTime)
+        with open(self.file['fileName'], "rb") as f:
+            # Go to the END
+            f.seek(0, os.SEEK_END)
+            for line in self.followingFile(f):
+                for match in self.iterLines(line):
+                    lineLogTime, linePid, lineType, lineDetail = match.groups()
+                    print(lineLogTime + "," + linePid + "," + lineType + "," + lineDetail)
+                    if(lineType == 'Stats'):
+                        for matchDetail in self.iterLinesDetailStats(lineDetail):
+                            lineReqBySec, lineBytesInBySec, lineBytesOutBySec, lineReqMicroSec = matchDetail.groups()
+                            print(BLUE + "Stats for the last minute : \n" +
+                                  CYAN + "Average requests by second : " + YELLOW + lineReqBySec +
+                                  CYAN + " Average bytes IN by second : " + YELLOW + lineBytesInBySec +
+                                  CYAN + " Average bytes OUT by second : " + YELLOW + lineBytesOutBySec +
+                                  CYAN + " Average request duration by second : " + YELLOW + str(float(lineReqMicroSec) / 1000000) + DEFAULT)
+                            self.updateStats(lineReqBySec, lineBytesInBySec, lineBytesOutBySec, lineReqMicroSec)
+                    elif(lineType[:4] == 'C-0x'):
+                        for matchDetail in self.iterLinesDetailLogin(lineDetail):
+                            lineDatabase, lineUser, lineIpFrom, linePortFrom, lineActionType, lineDbCx, lineUserCx, lineTls = matchDetail.groups()
+                            if(lineActionType == 'login'):
+                                print(GREEN + lineActionType + " : " + YELLOW + lineDbCx + CYAN + "/" + YELLOW + lineUserCx + CYAN + " from : " + YELLOW + lineIpFrom + DEFAULT)
+                                self.updateLogin(lineLogTime, lineDatabase, lineUser, lineIpFrom, linePortFrom, lineActionType, lineDbCx, lineUserCx, lineTls)
+                                # if(lineDb == 'ranger'):
+                                # analyzeLog(lastTime, logTime,logUser, logDatabase, logIpFrom, logPortFrom, logActionType, db, user, tls)
+                            # elif(logActionType == 'closing'):
+                            #     print(logActionType + " : " + logDatabase + "/" + logUser)
+                    else:
+                        print("Unknow line type : " + line)
+
+    def stop(self):
+        print(BLUE + "\nEnd date : " + DEFAULT + str(datetime.now().strftime(self.dateTimeFormat)))
+
 
 if __name__ == '__main__':
-    print(BLUE + "Start date : " + DEFAULT + str(actualTime))
-
-    with open("/var/log/pgbouncer/pgbouncer.log", "rb") as f:
-        f.seek(0,os.SEEK_END)
-        for line  in followingFile(f):
-#            print(line)
-            for match in iter_lines(line):
-                logTime, pid, lineType, lineDetail = match.groups()
-                if(lineType == 'Stats'):
-                    for matchDetail in iter_line_detail_Stats(lineDetail):
-                        logReqBySec, logBytesInBySec, logBytesOutBySec, logReqMicroSec = matchDetail.groups()
-                        print(BLUE + "Stats for the last minute : \n" + 
-                                    CYAN + "Average requests by second : " + YELLOW + logReqBySec + 
-                                    CYAN + " Average bytes IN by second : " + YELLOW + logBytesInBySec + 
-                                    CYAN + " Average bytes OUT by second : "  + YELLOW + logBytesOutBySec + 
-                                    CYAN + " Average request duration by second : "  + YELLOW + str(float(logReqMicroSec)/1000000) + DEFAULT)
-                elif(lineType[:4] == 'C-0x'):
-                    for matchDetail in iter_line_detail_Login(lineDetail):
-                        logUser, logDatabase, logIpFrom, logPortFrom, logActionType, db, user, tls = matchDetail.groups()
-                        if(logActionType == 'login'):
-                            print(GREEN + logActionType + " : " + DEFAULT + db + "/" + user + CYAN + " from : " + DEFAULT + logIpFrom)
-                            if(db == 'ranger'):
-                                analyzeLog(lastTime, logTime,logUser, logDatabase, logIpFrom, logPortFrom, logActionType, db, user, tls)
-                        # elif(logActionType == 'closing'):
-                        #     print(logActionType + " : " + logDatabase + "/" + logUser)
-                else:
-                    print("Unknow line type : " + line)
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    # Get the configuration file
+    with open('conf/LogAnalyzer.json') as jsonFile:
+        jsonData = json.load(jsonFile)
+    vg_configuration = jsonData
+    # Initiate the object
+    logAnalyzer = LogAnalyzer(vg_configuration)
+    try:
+        logAnalyzer.start()
+    except KeyboardInterrupt:
+        logAnalyzer.printStats()
+        logAnalyzer.printLogins()
+    finally:
+        logAnalyzer.stop()
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # LogAnalyzer
-A log analyzer
+
+
+A log analyzer for the logfile of pgbouncer
+
+You just have to set the path of the logfile in the *LogAnalyzer.json* file and run the *LogAnalyzer.py* script for collecting stats lines and all the logins stats.
+When you stop the programm a report is print like this one :
+
+```
+Stats for running periode from 2017-12-02 23:22:13 to 2017-12-02 23:22:38 : 
+Average requests by second : 0
+Average bytes IN by second : 0
+Average bytes OUT by second : 0
+Average request duration by second : 0.0
+
+Stats for logins, periode from 2017-12-02 23:22:13 to 2017-12-02 23:22:38 : 
+
+Account : postgres
+First login time : 2017-12-02 23:22:16
+Last login time : 2017-12-02 23:22:27
+Number of logins : 22
+Number of logins from 192.168.1.16 : 22
+
+End date : 2017-12-02 23:22:38
+
+```
